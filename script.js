4a65b786d6c51a71374c2863583c84561373f9cd
Maccio92 <valerio362@gmail.com>
2022-01-10 15:03:23 +0100

    add Mail List

---
+++ script.js
@@ -0,0 +1,31 @@
+/* Attraverso l’apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.*/
+
+let app = new Vue({
+    el: "#app",
+    data:{
+        mailList : [], 
+        mail: null,
+    },
+    created(){
+        let self = this;
+        for (let index = 0; index < 10; index++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(function (response) {   
+            console.log(response);
+            self.mail = response.data.response;
+            self.mailList.push(self.mail);
+            console.log(self.mailList);
+            })
+        .catch(function (error) {
+          console.log(error);
+        })   
+        }
+        
+    }
+}
+)
